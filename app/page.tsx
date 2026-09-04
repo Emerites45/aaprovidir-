a1d2817a43e62d67cce910bb333d192dcaf30097
yoantioma <yoan.tioma@aaprovidir.com>
2026-09-04 01:58:09 +0100

    travail fait à 70% version 9: architecture mise à jour

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import { HeroSupply } from "@/components/pages/HeroSupply/HeroSupply";
-import { EcoPromise } from "@/components/pages/EcoPromise/EcoPromise";
-import { HeroAnimated } from "@/components/pages/HeroAnimated/HeroAnimated";
-import { AccordionShowcase } from "@/components/pages/AccordionShowcase/AccordionShowcase";
-import { ProductsCarousel } from "@/components/pages/ProductsCarousel/ProductsCarousel";
-import { MobileFeatures } from "@/components/pages/MobileFeatures/MobileFeatures";
-import { StoryTimeline } from "@/components/pages/StoryTimeline/StoryTimeline";
-import { Faq } from "@/components/pages/Faq/Faq";
-import { Newsletter } from "@/components/pages/Newsletter/Newsletter";
+import { HeroSupply } from "@/components/pages/home/HeroSupply/HeroSupply";
+import { EcoPromise } from "@/components/pages/home/EcoPromise/EcoPromise";
+import { HeroAnimated } from "@/components/pages/home/HeroAnimated/HeroAnimated";
+import { AccordionShowcase } from "@/components/pages/home/AccordionShowcase/AccordionShowcase";
+import { ProductsCarousel } from "@/components/pages/home/ProductsCarousel/ProductsCarousel";
+import { MobileFeatures } from "@/components/pages/home/MobileFeatures/MobileFeatures";
+import { StoryTimeline } from "@/components/pages/home/StoryTimeline/StoryTimeline";
+import { Faq } from "@/components/pages/home/Faq/Faq";
+import { Newsletter } from "@/components/pages/home/Newsletter/Newsletter";
 
 export default function Home() {
   return (
